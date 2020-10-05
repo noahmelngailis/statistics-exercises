7f371cb50600fbc5b89c7d451fb5b97b8ca9bff3
Noah Melngailis <noah.melngailis@gmail.com>
2020-10-05 11:23:04 -0500

    Day 4

---
+++ 10_days_of_statistics.py
@@ -0,0 +1,32 @@
+# Day 4 Binomial Distribution
+
+def fact(n):
+    return 1 if n==0 else n*fact(n-1)
+
+def comb(n, x):
+    return fact(n) / (fact(x) * fact(n-x))
+
+def b(x, n, p):
+    return comb(n, x) * p**x * (1-p)**(n-x)
+
+l, r = list(map(float, input().split(" ")))
+odds = l / r
+
+print(round(sum([b(i, 6, odds / (1 + odds)) for i in range(3, 7)]), 3))
+
+
+# Day 4 Binomial Distributions 2
+
+def fact(n):
+    return 1 if n == 0 else n*fact(n-1)
+
+def comb(n, x):
+    return fact(n) / (fact(x) * fact(n-x))
+
+def b(x, n, p):
+    return comb(n, x) * p**x * (1-p)**(n-x)
+
+p, n = list(map(int, input().split(" ")))
+print(round(sum([b(i, n, p/100) for i in range(3)]), 3))
+print(round(sum([b(i, n, p/100) for i in range(2, n+1)]), 3))
+
