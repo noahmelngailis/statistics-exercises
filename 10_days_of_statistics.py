d8988ca0fb1aa24f422f3a0e5e89e5ad4a071311
Noah Melngailis <noah.melngailis@gmail.com>
2020-10-06 21:07:05 -0500

    geometric distribution

--- 10_days_of_statistics.py
+++ 10_days_of_statistics.py
@@ -30,3 +30,10 @@ p, n = list(map(int, input().split(" ")))
 print(round(sum([b(i, n, p/100) for i in range(3)]), 3))
 print(round(sum([b(i, n, p/100) for i in range(2, n+1)]), 3))
 
+# Day 4 Geometric Distributution
+num, den = list(map(int, input().split(" ")))
+n = int(input())
+
+p = num/den
+
+print(round((1-p)**(n-1)*p, 3))
